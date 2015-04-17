9eea13ece7d2b3a13238cd03a89e719f35f1c247
Sebastien Tronel <sebastien.tronel@gmail.com>
2015-04-17 23:32:12 +0200

    Correction bug code

--- js/grayscale.js
+++ js/grayscale.js
@@ -5,27 +5,27 @@
  */
 
 // jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+jQuery(window).scroll(function() {
+    if (jQuery(".navbar").offset().top > 50) {
+        jQuery(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        jQuery(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 });
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+jQuery(function() {
+    jQuery('a.page-scroll').bind('click', function(event) {
+        var jQueryanchor = jQuery(this);
+        jQuery('html, body').stop().animate({
+            scrollTop: jQuery(jQueryanchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
 });
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+jQuery('.navbar-collapse ul li a').click(function() {
+    jQuery('.navbar-toggle:visible').click();
 });
 
